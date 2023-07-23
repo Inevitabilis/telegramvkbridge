70c90afd66909d49b37352f09ff3bdadb96461b2
Inevitabilis <ddd1020018@gmail.com>
2023-07-23 14:15:12 +0300

    State machine initial

--- Program.cs
+++ Program.cs
@@ -4,12 +4,14 @@ using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Npgsql;
-using SharpVk;
-
+using VkNet;
+using telegramvkbridge;
 
 #region Initstuff
 string botToken = System.IO.File.ReadAllText("E:\\prog\\TgBotToken.txt");
-
+string sqlConnectionParams = System.IO.File.ReadAllText("E:\\prog\\sqlconnection.txt");
+var conn = new NpgsqlConnection(sqlConnectionParams);
+conn.Open();
 
 var botClient = new TelegramBotClient(botToken);
 using CancellationTokenSource cts = new();
@@ -41,23 +43,31 @@ Console.ReadLine();
 #region shutdown
 // Send cancellation request to stop bot
 cts.Cancel();
+conn.Close();
 #endregion
 
 
 async void Update()
 {
-    string sqlConnectionParams = System.IO.File.ReadAllText("E:\\prog\\sqlconnection.txt");
-    await using var conn = new NpgsqlConnection(sqlConnectionParams);
-    await conn.OpenAsync();
     await using (var cmd = new NpgsqlCommand("SELECT token FROM demodata WHERE dbid = 2", conn))
     await using (var reader = await cmd.ExecuteReaderAsync())
     {
         while (await reader.ReadAsync())
             Console.WriteLine(reader.GetString(0));
     }
+    await conn.CloseAsync();
+} 
+
+StaticStuff.UserState getUserState(Int64 userId)
+{
+    var reader = new NpgsqlCommand($"SELECT state FROM usertable WHERE id = {userId}").ExecuteReader();
+    if (reader.Read()) return (StaticStuff.UserState)reader.GetInt32(0);
+    else return StaticStuff.UserState.NoAuth;
 }
+void setUserState(Int64 userId, StaticStuff.UserState)
+{
 
-
+}
 
 async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
 {
@@ -70,6 +80,14 @@ async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, Cancel
         return;
 
     var chatId = message.Chat.Id;
+
+    if (messageText == "/start")    await botClient.SendTextMessageAsync(chatId,
+        "This bot mirrors your messages to the chat you specified from your account\n" +
+        "\n" +
+        "Your credentials would be required to sign in\n" +
+        "\n" +
+        "We do not store your credentials, only limited access tokens",
+        cancellationToken: cancellationToken);
 
     Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
 
---
+++ StaticStuff.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace telegramvkbridge
+{
+    internal static class StaticStuff
+    {
+        public enum UserState
+        {
+            NoAuth = 0,
+            EnteringLogin = 1,
+            EnteringPassword = 2,
+
+
+        }
+    }
+}
